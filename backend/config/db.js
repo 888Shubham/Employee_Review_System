13ab0a384b2e30faf786ab2986bd2fe41350b4e8
Shubham Kumar <133489048+888Shubham@users.noreply.github.com>
2024-02-24 19:49:26 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDB = async()=>{
     try {
-         await mongoose.connect("mongodb+srv://shubhamkb888:Shubham@123@cluster0.a4x67w4.mongodb.net/EmployeeReviewSystem");
+         await mongoose.connect("mongodb+srv://shubhamkb888:Shubham%40123@cluster0.a4x67w4.mongodb.net/EmployeeReviewSystem");
          console.log("MongoDB connected using mongoose");
     } catch (error) {
         console.log(error);
